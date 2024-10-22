eeaf0ff07c337e05d82e442db4297a5e21b00831
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2024-10-22 00:28:18 -0500

    se crea pagina de blog

--- src/pages/PostsList.tsx
+++ src/pages/PostsList.tsx
@@ -1,26 +1,67 @@
-// src/pages/PostsList.jsx
 import { Link } from "react-router-dom";
 
 const posts = [
-  { id: 1, title: "Primera publicación", excerpt: "Esta es una breve descripción de la primera publicación." },
-  { id: 2, title: "Segunda publicación", excerpt: "Esta es una breve descripción de la segunda publicación." },
-  { id: 3, title: "Tercera publicación", excerpt: "Esta es una breve descripción de la tercera publicación." },
+  {
+    id: 1,
+    title: "Primera publicación",
+    excerpt: "Esta es una breve descripción de la primera publicación.",
+    imgSrc: "assets/img/uriel-soberanes-MxVkWPiJALs-unsplash-2.png",
+  },
+  {
+    id: 2,
+    title: "Segunda publicación",
+    excerpt: "Esta es una breve descripción de la segunda publicación.",
+    imgSrc: "assets/img/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg",
+  },
+  {
+    id: 3,
+    title: "Tercera publicación",
+    excerpt: "Esta es una breve descripción de la tercera publicación.",
+    imgSrc: "assets/img/alexandru-acea-XEB8y0nRRP4-unsplash.jpg",
+  },
 ];
 
 function PostsList() {
   return (
-    <div className="posts-list">
-      <h1>Lista de Publicaciones</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.excerpt}</p>
-            <Link to={`/blog/${post.id}`}>Leer más</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="font-theseasons">
+        <section className="blogs-news-container">
+          <div className="grid-container blogs-main-new">
+            <div className="blogs-news-img-container">
+              <img
+                src="/foto-1.jpg"
+                alt="foto publicación principal"
+              />
+            </div>
+            <div className="blogs-news-info-container">
+              <h1>Titulo del Blogpost</h1>
+              <p>
+                Texto de intro. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Vitae ut natus voluptatum sint voluptatem, libero porro
+                ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Vitae ut natus voluptatum sint voluptatem, libero porro ratione
+              </p>
+              <a href={`/blog/${posts[0].id}`} className="blogs-button">Leer más</a>
+            </div>
+          </div>
+        </section>
+        <section className="blogs-post-container">
+          <div className="grid-container">
+            <h2 className="blogs-post-titulo-post">Publicaciones recientes</h2>
+            {posts.map((post) => (
+              <article key={post.id} className="post-container">
+                <img src={'/foto-1.jpg'} alt={post.title} />
+                <h3>{post.title}</h3>
+                <p>{post.excerpt}</p>
+                <Link to={`/blog/${post.id}`} className="blogs-button">
+                  Leer más
+                </Link>
+              </article>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
