0b16f5230ce43b0ba8dd2f75a94d6dd9b89c8050
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2024-11-08 03:57:29 -0500

    Obtiene los últimos 8 productos del JSON solo una vez

--- src/components/LatestProducts.tsx
+++ src/components/LatestProducts.tsx
@@ -2,10 +2,9 @@ import { FC } from "react";
 import ProductList from "./ProductList";
 import productosData from "../data/products.json";
 
-const LatestProducts: FC = () => {
-  // Obtiene los últimos 8 productos del JSON
-  const newProducts = productosData.slice(-8);
+const newProducts = productosData.slice(-8);
 
+const LatestProducts: FC = () => {
   return <ProductList title="Agregados recientemente" products={newProducts} />;
 };
 
