ee77c19e7f044b2b34852d95b9644f7e0a3928c8
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2024-11-08 14:58:24 -0500

    se corrige detalle del producto

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -36,14 +36,11 @@ const SingleProduct: FC = () => {
   }, [productID]);
 
   useEffect(() => {
-    const fetchPreferences = () => {
-      // Filtra productos similares por categoría
-      const similarProducts = productosData.filter(
-        (prod) => prod.category === sCategory && prod.id !== productID
-      );
-      setSimilar(similarProducts);
+    const getRandomProducts = (products: Product[], count: number): Product[] => {
+      const shuffled = [...products].sort(() => 0.5 - Math.random());
+      return shuffled.slice(0, count);
     };
-    if (sCategory) fetchPreferences();
+    setSimilar(getRandomProducts(productosData, 4));
   }, [productID, sCategory]);
 
   const addCart = () => {
@@ -100,33 +97,18 @@ const SingleProduct: FC = () => {
           </div>
         </div>
         <div className="px-2">
-          <h2 className="text-2xl">{product?.title}</h2>
-          <div className="mt-1">
-            {product?.discountPercentage && (
+          <h1 className="text-4xl font-bold mb-4">{product?.title}</h1>
+          <div className="mt-1 text-base">
+          c/u {product?.price} soles
+            {/* {product?.discountPercentage && (
               <PriceSection
                 discountPercentage={product.discountPercentage}
                 price={product.price}
               />
-            )}
+            )} */}
           </div>
-          <table className="mt-2">
-            <tbody>
-              <tr>
-                <td className="pr-2 font-bold">Marca</td>
-                <td>{product?.brand}</td>
-              </tr>
-              <tr>
-                <td className="pr-2 font-bold">Categoría</td>
-                <td>{product?.category}</td>
-              </tr>
-              <tr>
-                <td className="pr-2 font-bold">Stock</td>
-                <td>{product?.stock}</td>
-              </tr>
-            </tbody>
-          </table>
           <div className="mt-2">
-            <h2 className="font-bold">Sobre el producto</h2>
+            <h2 className="font-bold text-lg">Sobre el producto</h2>
             <p className="leading-5">
               {product?.description}
             </p>
@@ -134,7 +116,7 @@ const SingleProduct: FC = () => {
           <div className="flex flex-wrap items-center mt-4 mb-2 space-x-2">
             <button
               type="button"
-              className="flex items-center space-x-1 mb-2 hover:bg-pink-700 text-white p-2 rounded bg-pink-500"
+              className="flex items-center space-x-2 hover:bg-darkBg2 text-darkBg2 hover:text-white font-bold py-2 px-4 rounded border border-darkBg2 mb-2"
               onClick={addCart}
             >
               <AiOutlineShoppingCart />
@@ -142,7 +124,7 @@ const SingleProduct: FC = () => {
             </button>
             <button
               type="button"
-              className="flex items-center space-x-1 mb-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+              className="flex items-center space-x-2 hover:bg-darkBg2 text-darkBg2 hover:text-white font-bold py-2 px-4 rounded border border-darkBg2 mb-2"
               onClick={buyNow}
             >
               <FaHandHoldingDollar />
